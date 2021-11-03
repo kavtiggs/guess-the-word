1f2991b8816cfee1845304297012096ac6de17d8
kavtiggs <kavonna.tiggs@gmail.com>
2021-11-03 12:53:58 -0400

    Accept and validate guess

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
-const word = "magnolia"
+const word = "magnolia";
+const guessedLetters = [];
 
 //Display symbols for placeholder
 
@@ -24,7 +25,41 @@ const placeholder = function (word) {
 
   guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
+    // Single letter guess
+    const goodGuess = validateInput(guess);
+  
+    if (goodGuess) {
+      // Letter entered. Make a guess!
+      makeGuess(guess);
+    }
     letterInput.value = "";
   });
+
+  
+  //Validate player input
+
+  const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        // Is the input empty?
+        message.innerText = "Please enter a letter.";
+      } else if (input.length > 1) {
+        message.innerText = "Please enter a single letter.";
+      } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z.";
+      } else {
+        return input;
+      }
+  };
+
+  const makeGuess = function (guess) {
+      guess = guess.toUpperCase();
+      if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter. Please try again.";
+      } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+      }
+  };
